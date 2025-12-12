7f3c543b9dda25d2b75c4801b4f00fb9bb35d8cd
hazemDuff <hazemdafrawi@gmail.com>
2025-12-12 07:16:43 +0200

    Update Programming CW.py

--- Programming CW.py
+++ Programming CW.py
@@ -32,10 +32,63 @@ def Encoder(input_img, output_img):
         print("Can not accept empty string.") 
         return
 
-    f = open(input_img, 'rb')
-    imageData = list(f.read()) #The contents of the image
-    f.close()
-  
+    try:
+        f = open(input_img, 'rb')
+        imageData = list(f.read()) #The contents of the image
+        f.close()
+    except FileNotFoundError:
+        print("File entered does not exsist.")
+        return
+    
+    bits = int.from_bytes(imageData[28:30], byteorder='little') #bits per pixel
+    bytes = bits // 8 #bytes per pixel
+    header = int.from_bytes(imageData[10 : 14], byteorder='little') #stores the length of the header of the BMP image
+
+    if bytes < 3:
+        print("This image is not supported (must be either 24 or 32 bit image)")
+
+
+    messageBytes = message.encode('utf-8') #Convert the message using utf-8 encoding to bytes
+    messageBits = [] #empty list used to store the bits 
+
+    #converting each byte into 8 bits 
+    for byte in messageBytes: 
+        binary_string = format(byte, '08b') #this line converts the bytes into an 8 bit string using '08b' ensures that even empty spaces are filed with 0s
+        for bit in binary_string: 
+            messageBits.append(int(bit)) #here I convert the zeros and ones to inegers and stores them in the messageBits array
+
+    for i in range(8):
+        messageBits.append(0) #appends 8 zeros at the end message_bits to signal the end of the message (Delimiter)
+
+
+    pixels = (len(imageData) - header) // bytes #calculates total pixels in the image
+    freeBits = pixels * 3 #free space availble but this considers that each pixel contains 3 channels RGB (Alpha is skipped as it might affect the image greatly)
+    if len(messageBits) > freeBits: 
+        print("This message is too big for the image.")
+        print(f"Available bits: {freeBits}, \n required: {len(messageBits)}")
+        return
+
+    currBit = 0
+    currPixel = header # this stores the current pixel being edited starting after the byte_offset which is the header
+
+    while currBit < len(messageBits):
+        for channel in range(3): #loop through each channel (RGB)
+            if currBit >= len(messageBits):
+                break
+
+            imageData[currPixel + channel] = (imageData[currPixel + channel] & 0xFE) | messageBits[currBit] 
+            #the line above CLEARS the LSB and replaces with the current message bit by using an 'AND' and an 'OR' logic gates
+            currBit += 1 #move to the next bit in the RGB
+
+        currPixel += bytes #after we go through each channel and embed our message bit, we add 'bytes' to 'currPixel' to move to the next pixel in line
+    
+
+    try:
+        file = open(output_img, 'wb')
+        file.write(bytes(imageData)) #implements all of what we did in the new output file
+        print(f"You're message has been sucessfully hidden in {output_img}.")
+    except:
+        print("Error when writing to output image, please check file path permissions.")
      
 
 ###########################################################################################################################################################################
